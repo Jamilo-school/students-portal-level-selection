c82d9b740bc3d114d96b5525609dd63fda1dacf3
Jamilo-school <geofreyonyango167@gmail.com>
2023-08-21 16:59:46 +0300

    iiii

--- Class 8 biodata2023/app.js
+++ Class 8 biodata2023/app.js
@@ -17,47 +17,42 @@ inputs.forEach((input) => {
   input.addEventListener("blur", blurFunc);
 });
 
-// Start  of JavaScript code for generating notifications in order
 var notifications = [
-	"Good evening",
-	"Thanks for visiting our site.",
-	"Merry Christmas.",
-	"Happy New Year",
-	"Keep checking the website for updates.",
-  ];
-  
-  var notificationIndex = 0;
-  
-  function displayNotification() {
-	if (notificationIndex < notifications.length) {
-	  var notification = notifications[notificationIndex];
-  
-	  var notificationElement = document.createElement("div");
-	  notificationElement.classList.add("toast");
-	  notificationElement.innerText = notification;
-	  document.body.appendChild(notificationElement);
-  
-	  notificationElement.classList.add("toast-enter");
-	  setTimeout(function () {
-		notificationElement.classList.remove("toast-enter");
-		setTimeout(function () {
-		  notificationElement.classList.add("toast-exit");
-		  setTimeout(function () {
-			notificationElement.remove();
-			notificationIndex++;
-			displayNotification(); // Display the next notification
-		  }, 1000); // Exit animation duration
-		}, 5000); // Display duration
-	  }, 6000); // Entrance animation delay
-	} else {
-	  // All notifications have been displayed, reset the index
-	  notificationIndex = 0;
-	  setTimeout(displayNotification, 6000); // Delay before displaying the first notification again
-	}
+  "Other  class details .",
+  "Thanks for visiting our site.",
+ 
+];
+
+var notificationIndex = 0;
+
+function displayNotification() {
+  if (notificationIndex < notifications.length) {
+    var notification = notifications[notificationIndex];
+
+    var notificationElement = document.createElement("div");
+    notificationElement.classList.add("toast");
+    notificationElement.innerText = notification;
+    document.body.appendChild(notificationElement);
+
+    notificationElement.classList.add("toast-enter");
+    setTimeout(function () {
+      notificationElement.classList.remove("toast-enter");
+      setTimeout(function () {
+        notificationElement.classList.add("toast-exit");
+        setTimeout(function () {
+          notificationElement.remove();
+          notificationIndex++;
+          displayNotification(); // Display the next notification after the interval
+        }, 1000); // Exit animation duration
+      }, 5000); // Display duration
+    }, 6000); // Entrance animation delay
   }
-  
-  // Display the first notification and start the sequence
-  displayNotification();
+}
+
+// Start the sequence by displaying the first notification after an interval
+setTimeout(displayNotification, 2000); // Initial delay before displaying the first notification
+
+
 
   // End of JavaScript code for generating notifications in order
   
